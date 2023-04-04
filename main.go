e90fd6f174345a9a485e46f7d7e043dd14eddaee
xilaluna <xilaluna2@gmail.com>
2023-04-04 09:34:11 -0700

    add: .env to gitignore

--- main.go
+++ main.go
@@ -7,11 +7,11 @@ import (
 )
 
 func main() {
-	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
+	router := gin.Default()
+	router.GET("/ping", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
 			"message": "pong",
 		})
 	})
-	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+	router.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
