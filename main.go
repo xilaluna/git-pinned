72a48a22a6678fa8eed82aee01e9f8651ca08ae5
xilaluna <xilaluna2@gmail.com>
2023-04-04 09:37:35 -0700

    add: dotenv loading

--- main.go
+++ main.go
@@ -2,16 +2,20 @@ package main
 
 import (
 	"net/http"
+	"os"
 
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	godotenv.Load()
+
 	router := gin.Default()
 	router.GET("/ping", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
 			"message": "pong",
 		})
 	})
-	router.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+	router.Run("0.0.0.0:" + os.Getenv("PORT"))
 }
