b82511114b914b98ab24f8ca4d4a27dec51dd76b
xilaluna <xilaluna2@gmail.com>
2023-04-04 16:18:54 -0700

    add: scraping title and description

--- main.go
+++ main.go
@@ -1,21 +1,32 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 	"os"
 
 	"github.com/gin-gonic/gin"
+	"github.com/gocolly/colly"
 	"github.com/joho/godotenv"
 )
 
 func main() {
 	godotenv.Load()
-
 	router := gin.Default()
-	router.GET("/ping", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
+
+	router.GET("/ping", func(context *gin.Context) {
+		collector := colly.NewCollector()
+		collector.OnHTML(".pinned-item-list-item-content", func(content *colly.HTMLElement) {
+			title := content.ChildText("span.repo[title]")
+			description := content.ChildText("p.pinned-item-desc")
+			fmt.Printf("Pinned repo: %q\n", title)
+			fmt.Printf("Description: %q\n", description)
+		})
+		collector.Visit("https://github.com/xilaluna")
+		context.JSON(http.StatusOK, gin.H{
 			"message": "pong",
 		})
 	})
+
 	router.Run("0.0.0.0:" + os.Getenv("PORT"))
 }
