678e3979dccf90a6272db95893e79753e0ed524a
xilaluna <xilaluna2@gmail.com>
2023-04-05 22:45:35 -0700

    add::id for route

--- main.go
+++ main.go
@@ -20,20 +20,15 @@ type Repo struct {
 
 func scrape(link string) []Repo {
 	var repos []Repo
-
 	collector := colly.NewCollector(
 		colly.AllowedDomains("github.com"),
 	)
 	extensions.RandomUserAgent(collector)
-
 	collector.OnHTML(".pinned-item-list-item-content", func(content *colly.HTMLElement) {
-
 		title := content.ChildText("span.repo[title]")
-
 		description := content.ChildText("p.pinned-item-desc")
 		language := content.ChildText("span[itemprop=programmingLanguage]")
 		repoLink := fmt.Sprintf("https://github.com%s", content.ChildAttr("a", "href"))
-
 		repos = append(repos, Repo{Title: title, Description: description, Language: language, Link: repoLink})
 	})
 	collector.Visit(link)
@@ -46,6 +41,11 @@ func main() {
 
 	router.GET("/", func(context *gin.Context) {
 		repos := scrape("https://github.com/xilaluna")
+		context.IndentedJSON(http.StatusOK, repos)
+	})
+	router.GET("/:id", func(context *gin.Context) {
+		link := fmt.Sprintf("https://github.com/%s", context.Param("id"))
+		repos := scrape(link)
 		context.IndentedJSON(http.StatusOK, repos)
 	})
 
