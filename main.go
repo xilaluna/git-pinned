3ccde5aebce13b5f0e31518cc444975ea2200b84
xilaluna <xilaluna2@gmail.com>
2023-04-05 22:23:39 -0700

    add:scrape func

--- main.go
+++ main.go
@@ -18,29 +18,34 @@ type Repo struct {
 	Link        string `json:"link"`
 }
 
+func scrape(link string) []Repo {
+	var repos []Repo
+
+	collector := colly.NewCollector(
+		colly.AllowedDomains("github.com"),
+	)
+	extensions.RandomUserAgent(collector)
+
+	collector.OnHTML(".pinned-item-list-item-content", func(content *colly.HTMLElement) {
+
+		title := content.ChildText("span.repo[title]")
+
+		description := content.ChildText("p.pinned-item-desc")
+		language := content.ChildText("span[itemprop=programmingLanguage]")
+		repoLink := fmt.Sprintf("https://github.com%s", content.ChildAttr("a", "href"))
+
+		repos = append(repos, Repo{Title: title, Description: description, Language: language, Link: repoLink})
+	})
+	collector.Visit(link)
+	return repos
+}
+
 func main() {
 	godotenv.Load()
 	router := gin.Default()
 
 	router.GET("/", func(context *gin.Context) {
-		var repos []Repo
-
-		collector := colly.NewCollector(
-			colly.AllowedDomains("github.com"),
-		)
-		extensions.RandomUserAgent(collector)
-
-		collector.OnHTML(".pinned-item-list-item-content", func(content *colly.HTMLElement) {
-
-			title := content.ChildText("span.repo[title]")
-
-			description := content.ChildText("p.pinned-item-desc")
-			language := content.ChildText("span[itemprop=programmingLanguage]")
-			repoLink := fmt.Sprintf("https://github.com%s", content.ChildAttr("a", "href"))
-
-			repos = append(repos, Repo{Title: title, Description: description, Language: language, Link: repoLink})
-		})
-		collector.Visit("https://github.com/xilaluna")
+		repos := scrape("https://github.com/xilaluna")
 		context.IndentedJSON(http.StatusOK, repos)
 	})
 
