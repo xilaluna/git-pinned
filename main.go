0d6e17435d77e594c0c4ef8f3f7fb814c8fcbaa2
xilaluna <xilaluna2@gmail.com>
2023-04-05 21:21:46 -0700

    add: repo struct and send back json

--- main.go
+++ main.go
@@ -10,27 +10,33 @@ import (
 	"github.com/joho/godotenv"
 )
 
+type Repo struct {
+	Title       string `json:"title"`
+	Description string `json:"description"`
+	Language    string `json:"language"`
+	Link        string `json:"link"`
+}
+
 func main() {
 	godotenv.Load()
 	router := gin.Default()
 
 	router.GET("/ping", func(context *gin.Context) {
+		var repos []Repo
+
 		collector := colly.NewCollector()
 		collector.OnHTML(".pinned-item-list-item-content", func(content *colly.HTMLElement) {
+
 			title := content.ChildText("span.repo[title]")
+
 			description := content.ChildText("p.pinned-item-desc")
 			language := content.ChildText("span[itemprop=programmingLanguage]")
-			repoLink := content.ChildAttr("a", "href")
+			repoLink := fmt.Sprintf("https://github.com%s", content.ChildAttr("a", "href"))
 
-			fmt.Printf("Pinned repo: %q\n", title)
-			fmt.Printf("Description: %q\n", description)
-			fmt.Printf("Language: %q\n", language)
-			fmt.Printf("Link: %q\n", repoLink)
+			repos = append(repos, Repo{Title: title, Description: description, Language: language, Link: repoLink})
 		})
 		collector.Visit("https://github.com/xilaluna")
-		context.JSON(http.StatusOK, gin.H{
-			"message": "pong",
-		})
+		context.IndentedJSON(http.StatusOK, repos)
 	})
 
 	router.Run("0.0.0.0:" + os.Getenv("PORT"))
