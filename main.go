3862183025bf7a67eb8a188e0fd88b199864c793
xilaluna <xilaluna2@gmail.com>
2023-04-05 20:31:41 -0700

    update:scrape language and link

--- main.go
+++ main.go
@@ -19,8 +19,13 @@ func main() {
 		collector.OnHTML(".pinned-item-list-item-content", func(content *colly.HTMLElement) {
 			title := content.ChildText("span.repo[title]")
 			description := content.ChildText("p.pinned-item-desc")
+			language := content.ChildText("span[itemprop=programmingLanguage]")
+			repoLink := content.ChildAttr("a", "href")
+
 			fmt.Printf("Pinned repo: %q\n", title)
 			fmt.Printf("Description: %q\n", description)
+			fmt.Printf("Language: %q\n", language)
+			fmt.Printf("Link: %q\n", repoLink)
 		})
 		collector.Visit("https://github.com/xilaluna")
 		context.JSON(http.StatusOK, gin.H{
